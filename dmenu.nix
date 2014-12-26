61c11bfbb7230f861e5dfe3b8b95271c1405299d
anthony cantor <ar.cantor@gmail.com>
2014-12-26 02:30:48 -0800

    initial commit

---
+++ dmenu.nix
@@ -0,0 +1,21 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "cantora-dmenu";
+
+  src  = fetchgit {
+    url     = "https://github.com/cantora/dmenu.git";
+    rev     = "refs/heads/master";
+    sha256  = "0ajy0xsgg5k1gb91rgqac5agpsi2laxf26p4vm14hwaj2lgikppq";
+  };
+
+  buildInputs = with xlibs; [ libX11 libXinerama ];
+
+  prePatch = ''
+    sed -re 's!\<(dmenu|dmenu_path)\>!'"$out/bin"'/&!g' dmenu_run \
+      && sed -re 's!\<stest\>!'"$out/bin"'/&!g' dmenu_path \
+      && sed -i "s@PREFIX = /usr/local@PREFIX = $out@g" config.mk
+  '';
+
+  buildPhase = " make ";
+}
---
+++ dwm.nix
@@ -0,0 +1,19 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "cantora-dwm";
+
+  src = fetchgit {
+    url       = "https://github.com/cantora/dwm.git";
+    rev       = "refs/heads/cantora";
+    sha256    = "0dv0z365qbl9g97m2zsd42c5j20barfgx3rz8q449spx6ifz404y";
+  };
+
+  buildInputs = with xlibs; [ libX11 libXinerama ];
+
+  prePatch = ''
+    sed -i "s@PREFIX = /home/\$(USER)@PREFIX = $out@" config.mk
+  '';
+
+  buildPhase = " make ";
+}
